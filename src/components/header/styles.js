6f59accbde64058311f9d57741449a6ab957e3e2
Pews-DEV <pedro.silvaifce@gmail.com>
2022-09-03 01:44:36 -0300

    feat: ajustando header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -7,19 +7,27 @@ export const Header = styled.header`
 
   display: flex;
   align-items: center;
-  justify-content: flex-end;
-  gap: 40px;
+  justify-content: center;
+  gap: 20px;
 
   width: 100%;
   height: 60px;
 
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  @media (min-width: 426px) {
+    justify-content: flex-start;
+  }
 `
 
 export const Link = styled.a`
   font-family: 'Inter';
   font-style: normal;
   font-weight: 700;
-  font-size: 14px;
+  font-size: 10px;
   color: white;
+
+  @media (min-width: 426px) {
+    font-size: 14px;
+  }
 `;
