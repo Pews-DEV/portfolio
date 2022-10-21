e98eb8adf3d5e0493110bf5df8bc09646865cd0e
Pews-DEV <pedro.silvaifce@gmail.com>
2022-10-20 21:11:12 -0300

    refactor: renomeando arquivo da home

---
+++ src/components/home/index.jsx
@@ -0,0 +1,28 @@
+import totoro from '../../assets/totoro.png'
+import amigos from '../../assets/amigos.png'
+import * as S from './styles'
+
+const Home = () => {
+  return(
+    <S.Container>
+      <S.Introduction>
+        <S.TextContainer>
+          <S.Welcome>BEM VINDOS,</S.Welcome>
+          <S.Nipon src={amigos} alt="amigos" />
+        </S.TextContainer>
+        <S.TextContainer>
+          <S.Text>SOU O PEWS,</S.Text>
+          <S.Text>AMANTE DE TECNOLOGIA</S.Text>
+          <S.Text>E ESSE É MEU</S.Text>
+          <S.Text>HUMILDE</S.Text>
+          <S.Text>PORTFÓLIO</S.Text>
+        </S.TextContainer>
+      </S.Introduction>
+      <S.ImgContainer>
+        <S.Image src={totoro} alt="totoro" />
+      </S.ImgContainer>
+    </S.Container>
+  )
+};
+
+export default Home;
